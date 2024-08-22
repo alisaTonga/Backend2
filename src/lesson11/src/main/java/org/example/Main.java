953c6059ef73ec5e3b593b35519799741ac19d63
alisaTonga <tongalise@gmail.com>
2024-08-22 15:36:31 +0200

    lesson12

--- src/lesson11/src/main/java/org/example/Main.java
+++ src/lesson11/src/main/java/org/example/Main.java
@@ -10,5 +10,13 @@ public class Main {
         UserXmlHibernateConfiguration configuration = new UserXmlHibernateConfiguration(hibernateConfiguration);
         List<User> users = configuration.findAll();
         System.out.println(users);
+
+        System.out.println("Find by id");
+        User user = configuration.getByID(2L);
+        System.out.println(user);
+
+        System.out.println("add new user");
+        User newUser= configuration.create(new User(null, "George" , "george@gmail.com", "bulfsgfafg"));
+        System.out.println(newUser);
     }
 }
--- src/lesson11/src/main/java/org/example/UserXmlHibernateConfiguration.java
+++ src/lesson11/src/main/java/org/example/UserXmlHibernateConfiguration.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 import org.example.enitity.User;
 
 import java.util.List;
@@ -16,5 +17,26 @@ public class UserXmlHibernateConfiguration {
     public List<User> findAll() {
         return entityManager.createQuery("from User", User.class).getResultList();
         //manager create request from user
+    }
+
+    public User getByID(Long id) {
+        return entityManager.find(User.class, id);
+    }
+
+    public User create(User user) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        try{
+            transaction.begin();
+            entityManager.persist(user);
+            transaction.commit();
+            return user;
+        }
+        catch(Exception e){
+            if (transaction !=null && transaction.isActive()) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Error creating user" + user);
+
+        }
     }
 }
